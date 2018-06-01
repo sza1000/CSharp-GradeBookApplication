47fe23036fe41f5cbd434f8bc7f2efccf7709b1c
sza1000 <shaunismd@gmail.com>
2018-05-31 20:19:03 -0400

    RankedGradebook Overloads

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,5 +32,29 @@ namespace GradeBook.GradeBooks
             
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked Grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+
+                base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked Grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            else
+
+            base.CalculateStudentStatistics(name);
+        }
+
     }
 }
